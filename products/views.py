e9b02048b11d1d27dee86fef0bf8bd1b59599d00
Pablo Hurtado <administracione@phdsystems.net>
2024-07-09 19:14:31 -0600

    Se crea la estructura del proyecto con Django y se crea la aplicación poducts. Se agregan el modelo, la vista y los templates de product. Se crea el template base.html y se configura para utilizar Tailwinds css con CDN

---
+++ products/views.py
@@ -0,0 +1,22 @@
+from django.urls import reverse_lazy
+from django.views import generic
+
+from products.models import Product
+from .forms import ProductForm
+
+class ProductFormView(generic.FormView):
+    template_name = 'products/add_product.html'
+    form_class = ProductForm
+    success_url = reverse_lazy('add_product')
+
+    
+    def form_invalid(self, form):
+        form.save()
+        return super().form_invalid(form)
+    
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/list_product.html'
+    context_object_name = 'products'
+    
